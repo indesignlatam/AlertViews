c8a49773623a2500938518ccee7b08a23744d40a
Paulo Mogollon <wanchopeblanco@gmail.com>
2016-04-14 18:15:19 -0500

    Added github repository

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'indesign:alertviews',
-  version: '0.0.2',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: 'AlertViews: iOS like AlertViews',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/wanchopeblanco/AlertViews',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
